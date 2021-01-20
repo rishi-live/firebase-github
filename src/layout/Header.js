be5dc9b3fe2a6fa6fceeea1ee7baf71784377791
rishi-live <rishi.uemk9@gmail.com>
2021-01-21 00:07:29 +0530

    layout

---
+++ src/layout/Header.js
@@ -0,0 +1,66 @@
+import React, { useState, useContext } from "react";
+import {
+  Container,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavbarText,
+  NavLink,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { UserContext } from "../context/UserContext";
+
+const Header = () => {
+  const context = useContext(UserContext);
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <Navbar color="info" light expand="md">
+      <NavbarBrand>
+        <Link to="/" className="text-white">
+          Rishi
+        </Link>
+      </NavbarBrand>
+      <NavbarText className="text-white">
+        {context.user?.email ? context.user.email : ""}
+      </NavbarText>
+      <NavbarToggler onClick={toggle} />
+      <Collapse isOpen={isOpen} navbar>
+        <Nav className="ml-auto" navbar>
+          {context.user ? (
+            <NavItem>
+              <NavLink
+                onClick={() => {
+                  context.setUser(null);
+                }}
+                className="text-white"
+              >
+                Logout
+              </NavLink>
+            </NavItem>
+          ) : (
+            <>
+              <NavLink tag={Link} to="/signup" className="text-white">
+                Signup
+              </NavLink>
+
+              <NavItem>
+                <NavLink tag={Link} to="/signin" className="text-white">
+                  Signin
+                </NavLink>
+              </NavItem>
+            </>
+          )}
+        </Nav>
+      </Collapse>
+    </Navbar>
+  );
+};
+
+export default Header;
